225311880e781d3a34b7a230519b5ca9a420fded
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-23 02:23:07 +0000

    clientForm

--- client/src/components/AddClientForm.js
+++ client/src/components/AddClientForm.js
@@ -1,9 +1,68 @@
-
+import { useState } from "react";
 
 const AddClientForm = () => {
-  return (
-    <div>AddClientForm</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-export default AddClientForm
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const data = {
+      name,
+      email,
+      phone,
+    };
+    console.log(data);
+  };
+
+  return (
+    <form
+      onSubmit={submitHandler}
+      className="w-1/2 mx-auto flex flex-col gap-y-6 items-center justify-center p-4 border rounded-md shadow-lg"
+    >
+      <div className="flex flex-col gap-y-2 w-full">
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          id="name"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col gap-y-2 w-full">
+        <label htmlFor="email">email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          id="email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col gap-y-2 w-full">
+        <label htmlFor="phone">phone</label>
+        <input
+          type="text"
+          name="phone"
+          value={phone}
+          id="phone"
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddClientForm;
