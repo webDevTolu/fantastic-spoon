7b22c174fdf72c16ac07231d20bfd5ceacc0943d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-25 12:39:57 +0000

    addProject mutation

--- client/src/components/AddProjectForm.js
+++ client/src/components/AddProjectForm.js
@@ -1,4 +1,10 @@
+import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { ADD_PROJECT } from "../mutations/projectMutation";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
+import ErrorNotification from "./ui/ErrorNotification";
+import Loading from "./ui/Loading";
 
 const AddProjectForm = () => {
   const [name, setName] = useState("");
@@ -6,6 +12,26 @@ const AddProjectForm = () => {
   const [clientID, setClientID] = useState("");
   const [status, setStatus] = useState("Not Started");
   const [disable, setDisable] = useState(true);
+
+  // fetches client for clientID in the form
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  // add project mutation
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: {
+      name,
+      description,
+      status,
+      clientID,
+    },
+    update(cache, { data: { addProject } }) {
+      const { allProjects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { allClients: [...allProjects, addProject] }, // adds new client to the rests of the clients list
+      });
+    },
+  });
 
   // simple form validation
   useEffect(() => {
@@ -18,8 +44,13 @@ const AddProjectForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(status);
+
+    // sends data to server
+    addProject(name, description, clientID, status)
   };
+
+  if (loading) return <Loading />;
+  if (error) return <ErrorNotification />;
 
   return (
     <form
@@ -39,13 +70,21 @@ const AddProjectForm = () => {
 
       <div className="flex flex-col gap-y-2 w-full">
         <label htmlFor="clientID">clientID</label>
-        <input
-          type="text"
-          name="clientID"
-          value={clientID}
-          id="clientID"
-          onChange={(event) => setClientID(event.target.value)}
-        />
+        {!loading && !error && (
+          <select
+            name="clientID"
+            id="clientID"
+            value={clientID}
+            onChange={(event) => setClientID(event.target.value)}
+          >
+            {/* maps all the clients and return the id when selected */}
+            {data.allClients.map((client) => (
+              <option key={client.id} value={client.id}>
+                {client.name}
+              </option>
+            ))}
+          </select>
+        )}
       </div>
 
       <div className="flex flex-col gap-y-2 w-full">
@@ -56,9 +95,9 @@ const AddProjectForm = () => {
           value={status}
           onChange={(event) => setStatus(event.target.value)}
         >
-          <option value="Not Started">Not Started</option>
-          <option value="In Progress">In Progress</option>
-          <option value="Completed">Completed</option>
+          <option value="NEW">Not Started</option>
+          <option value="PROGRESS">In Progress</option>
+          <option value="COMPLETED">Completed</option>
         </select>
       </div>
 
--- client/src/mutations/projectMutation.js
+++ client/src/mutations/projectMutation.js
@@ -1,26 +1,19 @@
 import { gql } from "@apollo/client";
 
-export const CREATE_PROJECT = gql`
-mutation addProject($name: String!, $description: String!, ) {}
+export const ADD_PROJECT = gql`
+mutation addProject($name: String!, $description: String!, $status: ProjectStatus!, $clientID: ID!) {
+  addProject(name: $name, description: $description, status:
+  $status, clientID: $clientID) {
+    id
+    name
+    description
+    status
+    client {
+      id
+      name
+      email
+      phone
+    }
+  }
+}
 `;
-
-//  mutation deleteClient($id: ID!) {
-//     deleteClient(id: $id) {
-//       id
-//       name
-//       email
-//       phone
-//     }
-//   }
-
-// mutation {
-//   addProject(name: "new project", description: "new description", status: PROGRESS, clientId: "62dac5665d8716b5fe6e4d46") {
-//     id
-//     name
-//     status
-//     description
-//     client {
-//       name
-//     }
-//   }
-// }
